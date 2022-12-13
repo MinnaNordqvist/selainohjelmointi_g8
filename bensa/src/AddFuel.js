8729355b0de938d3a6d333ae2057f59377b8cf1c
Minna <minna.nordqvist@edu.turkuamk.fi>
2022-12-13 15:37:50 +0200

    Lisätty radiobutton sähkölle

--- bensa/src/AddFuel.js
+++ bensa/src/AddFuel.js
@@ -1,7 +1,6 @@
 import React, {useState, useContext} from 'react';
 import { GlobalContext } from './GlobalState';
 
-
 var num = 1;
 
 export const AddFuel = () => {
@@ -9,7 +8,14 @@ export const AddFuel = () => {
   var[litrat, setLitra] = useState(0);
   var[hinta, setHinta] = useState(0);
   var[km, setKm] = useState(0);
+  var[kwh, setSahko] = useState(0);
+  var[tyyppi, valitseTyyppi] = useState("litrat");
   const{addTransaction} = useContext(GlobalContext);
+
+  const choose = (event) =>{
+    event.preventDefault();
+    valitseTyyppi(event.target.value);
+  };
   
   const onSubmit = e => {
     e.preventDefault();
@@ -17,6 +23,7 @@ export const AddFuel = () => {
       id: num,
       text,
       litrat: +litrat,
+      kwh: +kwh,
       hinta: +hinta,
       km: +km
     }
@@ -26,14 +33,24 @@ export const AddFuel = () => {
     setLitra(0);
     setHinta(0);
     setKm(0);
+    setSahko(0);
   }
     return (
     <div>
     <h3>Tankkauskerta</h3>
     <form onSubmit={onSubmit}>
-    <label htmlFor='litrat'>Kuinka monta litraa tankattiin?</label>
-    <br />
-    <input type="number" value={litrat} onChange={(e) => setLitra(e.target.value)} placeholder="litrat"/>
+    <div className="radio">
+    <label>Bensa vai sähkö?</label>
+    <input type="radio" checked={tyyppi === "litrat"} value="litrat" label="litrat" onChange={choose}/>bensa 
+    <input type="radio" checked={tyyppi === "kwh"} value="kwh" label="kwh" onChange={choose}/>sähkö
+    </div>  
+    <br/>
+    {tyyppi === "litrat" && (
+    <><label htmlFor='litrat'>Kuinka monta litraa tankattiin?</label><br /><input type="number" value={litrat} onChange={(e) => setLitra(e.target.value)} placeholder="litrat" /></>
+    )}
+    {tyyppi === "kwh" && (
+    <><label htmlFor='kwh'>Kuinka monta kilowattituntia ladattiin?</label><br /><input type="number" value={kwh} onChange={(e) => setSahko(e.target.value)} placeholder="kwh" /></>
+    )}
     <br />
     <label>Kuinka paljon maksoi?</label>
     <br />
